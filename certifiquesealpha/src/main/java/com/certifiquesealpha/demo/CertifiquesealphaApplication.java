ae6c4a39f79e8083ab9bf90f6e241dd5f0364929
SoHaiNp <139929891+SoHaiNp@users.noreply.github.com>
2024-07-30 14:48:19 -0300

    Start
    Start do projeto. Spring Data JPA, PostgreSQL, Spring Dev Tools, Spring Web.

---
+++ certifiquesealpha/src/main/java/com/certifiquesealpha/demo/CertifiquesealphaApplication.java
@@ -0,0 +1,13 @@
+package com.certifiquesealpha.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifiquesealphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifiquesealphaApplication.class, args);
+	}
+
+}
